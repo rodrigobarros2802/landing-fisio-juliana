be8a2e9bd4d0c01740e670dd7bdf2e1f0b66a430
Rodrigo Barros <rodrigobarros2802@gmail.com>
2023-08-09 21:49:25 -0300

    navegando em javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 const botao = document.querySelector(".btn-fixed");
 
 botao.addEventListener ("click", function () {
-    window.scrollTo(0,0), Animation;
+    window.scrollTo(0,0);
 });
 
 window.addEventListener ("scroll", function () {
