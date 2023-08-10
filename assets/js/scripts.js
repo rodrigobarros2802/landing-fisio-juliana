8beee7472bc4296a70dec01f4b210fb492a52f22
Rodrigo Barros <rodrigobarros2802@gmail.com>
2023-08-09 21:24:03 -0300

    javascript funcionando btn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,11 @@ const botao = document.querySelector(".btn-fixed");
 botao.addEventListener ("click", function () {
     window.scrollTo(0,0);
 });
+
+window.addEventListener ("scroll", function () {
+    if (window.scrollY == 0) {
+        botao.classList.remove("visible");
+    } else {
+        botao.classList.add("visible");
+    }
+});
